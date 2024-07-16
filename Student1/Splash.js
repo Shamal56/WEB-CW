5d5ab7769dc69dc0b7793dfb663e10b0b296e407
Shamal Jayawardena <shamal.20230937@iit.ac.lk>
2024-07-16 23:29:20 +0530

    version 2.5

--- Student1/Splash.js
+++ Student1/Splash.js
@@ -1,5 +1,6 @@
+//redirect to the home page
 function Home() {
-    window.location.href = "Shop.html"
+    window.location.href = "Home.js"
 }
-
+//time for the splash screen
 setTimeout(Home, 4000);
